9c2132f2f2c55b61d89d2e9af2200b955dd97f19
Eric <ericlingren@gmail.com>
2019-01-04 13:55:05 -0700

    full routing built into the nav bar and app file.  core componets also built.

---
+++ client/src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+  class Cart extends Component{
+    render(){
+        return (
+            <div>
+                Put your shopping cart here
+            </div>
+        );
+    }
+};
+
+export default Cart;
--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import { Card } from 'reactstrap';
+import ProductCard from './ProductCard'
 
   class Store extends Component{
     render(){
@@ -7,6 +8,9 @@ import { Card } from 'reactstrap';
             <div style={{width: '90%'}}>
                 <Card>
                    This is the store component.  Add products and set up grids in here.
+                   <ProductCard />
+                   <ProductCard />
+                   <ProductCard />
                 </Card>
             </div>
         );
