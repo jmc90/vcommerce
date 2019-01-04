9df10417f6516bde6c9542fb21b7b373e7bc112c
Eric <ericlingren@gmail.com>
2019-01-04 11:14:53 -0700

    login form template component built.  No handle functions

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,1 +1,23 @@
-// login form here
+import React, { Component} from 'react';
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Form >
+          <h3>Login:</h3>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm
