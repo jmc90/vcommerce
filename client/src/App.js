dc46d2cbd93ed8b5fd6680847c1d7732b9c2ed36
Eric <ericlingren@gmail.com>
2019-01-04 10:31:18 -0700

    navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import NavbarComponent from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
-       
+        <NavbarComponent/>
       </div>
     )
   }
