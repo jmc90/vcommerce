7c1db3c338eb9600818ad4cf7de09e168cc38825
Scott <cadidadi@gmail.com>
2019-01-05 14:11:30 -0700

    footer & app.css

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,13 @@ import About from './components/About'
 import Cart from './components/Cart'
 import Footer from './components/Footer/Footer'
 import { Switch, Route } from "react-router-dom";
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='main'>
         <NavbarComponent/>
-
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/store" component={Store} />
@@ -25,7 +25,6 @@ class App extends Component {
             <Route path="/profile" component={Profile} />
             <Route path="/cart" component={Cart} />
         </Switch>
-
         <Footer />
       </div>
     )
