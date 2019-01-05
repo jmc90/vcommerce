4abdff3354575a0683335810b6c7d682471b3907
Scott <cadidadi@gmail.com>
2019-01-05 14:37:44 -0700

    footer update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,42 +3,8 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class="flex-row">
-      <ul class="footer-list-top">
-        <li>
-          <h4 class="footer-list-header">About Us</h4>
-        </li>
-      </ul>
-      <ul class="footer-list-top">
-        <li>
-          <h4 class="footer-list-header">Products</h4>
-        </li>
-        <li>
-          <a href="/xxxx" class="generic-anchor footer-list-anchor">
-            GPU's
-          </a>
-        </li>
-      </ul>
-      <ul class="footer-list-top">
-        <li id="help">
-          <h4 class="footer-list-header">Contact Us</h4>
-        </li>
-        <li>
-          <a
-            href="/xxxx"
-            class="generic-anchor footer-list-anchor"
-          >
-            CONTACT
-          </a>
-        </li>
-      </ul>
-      <section class="footer-bottom-section flex-row">
-        <div class="footer-bottom-wrapper">Copyright 2019
-        </div>
-        <div class="footer-bottom-wrapper">
-          
-        </div>
-      </section>
+    <footer class="d-flex justify-content-center align-items-center">
+      <p className="m-0">Copyright &copy;2019</p>
     </footer>
   );
 };
