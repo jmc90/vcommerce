e6ae20ef73403d76aaecbe8c174e527cc32e308f
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2019-01-06 15:13:37 -0700

    base wishlist add to list functionality complete

--- client/src/components/Wishlist/Wishlist.js
+++ client/src/components/Wishlist/Wishlist.js
@@ -1,13 +1,46 @@
 import React, { Component} from 'react';
+import { withProducts } from '../../context/ProductProvider';
 
   class Wishlist extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
     render(){
+
+        let row = this.props.currentWishlist.map(item => {
+            let title = item.title
+            let price = item.price
+            let quantity = item.quantity
+            
+            return (
+                <tr>
+                    <td> { title } </td>
+                    <td> ${ price } </td> 
+                    <td> { quantity } </td>
+                    <td> ${ quantity * price } </td>
+                    <button>remove item</button>
+                </tr>
+            )
+        })
+
         return (
             <div>
-                Build your wishlist here
+                <table >
+                    <tr>
+                        <th>Item</th>
+                        <th>Price</th> 
+                        <th>Quantity</th>
+                        <th>Total</th>
+                    </tr>
+                    {row}
+                </table>
             </div>
         );
     }
 };
 
-export default Wishlist;
+export default withProducts(Wishlist);
