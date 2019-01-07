5befe9ce2194abaa4259a208c14fdf1194665928
Scott <cadidadi@gmail.com>
2019-01-07 13:22:57 -0700

    video overlay

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
-import CryptoMiningClip from '../..//styles/media/crypto-mining-lg.mp4'
+import React from "react";
+// import { Jumbotron, Container } from 'reactstrap';
+import CryptoMiningClip from "../..//styles/media/crypto-mining-lg.mp4";
+import "./HomePage.css";
 
 const HomePage = () => {
-    return (
-      <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <video autoPlay muted loop id="myVideo" width='100%' >
-            <source src={CryptoMiningClip} type="video/mp4"/>
-          </video>
-          <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </Container>
-      </Jumbotron>
-      </div>
-    )
-}
+  return (
+    <div className="content">
+      <video
+        autoPlay
+        muted
+        loop
+        id="myVideo"
+        width="100%"
+      >
+        <source src={CryptoMiningClip} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
 
 export default HomePage;
