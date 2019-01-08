3175f47cbf8ca290657da86ca1244a85a9f4e5ce
Scott <cadidadi@gmail.com>
2019-01-08 12:16:45 -0700

    product card style

--- client/src/components/Store/ProductCard.js
+++ client/src/components/Store/ProductCard.js
@@ -10,33 +10,38 @@ import {
 import "./productCardStyle.css";
 import { Link } from "react-router-dom";
 
-const ProductCard = ({title, price, description, image, sku}) => {
-      return (
-        <div style={{ width: "318px" }} className="productCard">
-          <Card>
-            <CardImg
-              top
-              width="100%"
-              height="200px"
-              src={image}
-              alt="Card image cap"
-            />
-            <CardBody>
-              <h4> {title} </h4>
-              <CardSubtitle> ${price} </CardSubtitle>
-              <CardText> {description} </CardText>
-              <div className="buttonContainer">
-                <Link to={`/store/${sku}`}>
-                  <Button>
-                    View Details
-                  </Button>
-                </Link>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
-    )
-}
+const ProductCard = ({ title, price, description, image, sku }) => {
+  return (
+    <div style={{ width: "318px" }} className="productCard">
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          height="200px"
+          src={image}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <h4> {title} </h4>
+          <CardSubtitle> ${price} </CardSubtitle>
+          <CardText> {description} </CardText>
+          <div className = "buttons-container"> 
+            <div className="buttonContainer">
+              <Link to={`/store/${sku}`}>
+                <Button>View Details</Button>
+              </Link>
+            </div>
+            <div className="buttonContainer">
+              {/* set this link to go directly to shopping cart */}
+              <Link to={`/store/${sku}`}>
+                <Button>Buy Now</Button>
+              </Link>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
-
-export default ProductCard
+export default ProductCard;
