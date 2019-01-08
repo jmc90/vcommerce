7bfab5a2c2de19ca799bd8defe58853e0a481c93
Scott <cadidadi@gmail.com>
2019-01-08 11:39:00 -0700

    header & footer

---
+++ client/src/components/Calculators/Calculators.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Calculators = () => {
+    return(
+        <div>xxx Calculators here
+        </div>
+    )
+}
+
+export default Calculators
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,51 +2,51 @@ import React from "react"
 import "./Footer.css"
 
 const Footer = () => {
-  return (
+  return ( 
 
-    <footer id="vcommFooter">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-3 myCols">
+    <footer className="vcommFooter">
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-3 myCols">
                     <h5>Get started</h5>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Sign up</a></li>
-                        <li><a href="#">Downloads</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/register">Sign up</a></li>
+                        <li><a href="/login">Login</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3 myCols">
+                <div className="col-sm-3 myCols">
                     <h5>About us</h5>
                     <ul>
-                        <li><a href="#">Company Information</a></li>
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Reviews</a></li>
+                        <li><a href="/">Company Info</a></li>
+                        <li><a href="/contact">Contact us</a></li>
+                        <li><a href="/">Reviews</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3 myCols">
+                <div className="col-sm-3 myCols">
                     <h5>Support</h5>
                     <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Help desk</a></li>
-                        <li><a href="#">Forums</a></li>
+                        <li><a href="/">FAQ</a></li>
+                        <li><a href="/">Help desk</a></li>
+                        <li><a href="/">Forums</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3 myCols">
+                <div className="col-sm-3 myCols">
                     <h5>Legal</h5>
                     <ul>
-                        <li><a href="#">Terms of Service</a></li>
-                        <li><a href="#">Terms of Use</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="/">Terms of Service</a></li>
+                        <li><a href="/">Terms of Use</a></li>
+                        <li><a href="/">Privacy Policy</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-        <div class="social-networks">
-            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-            <a href="#" class="facebook"><i class="fa fa-facebook-official"></i></a>
-            <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+        <div className="social-networks">
+            <a href="/" className="twitter"><i className="fa fa-twitter"></i></a>
+            <a href="/" className="facebook"><i className="fa fa-facebook-official"></i></a>
+            <a href="/" className="google"><i className="fa fa-google-plus"></i></a>
         </div>
-        <div class="footer-copyright">
+        <div className="footer-copyright">
             <p>© 2019 Copyright Crypto Minerz </p>
         </div>
     </footer>
