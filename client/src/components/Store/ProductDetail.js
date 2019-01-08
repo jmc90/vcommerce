e27702ce6652129f3c7b8f23d04108eb703689cb
Eric <ericlingren@gmail.com>
2019-01-08 11:49:09 -0700

    more styling on product details page

--- client/src/components/Store/ProductDetail.js
+++ client/src/components/Store/ProductDetail.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import { withProducts } from '../../context/ProductProvider'
 import { Button } from 'reactstrap';
+// import { url } from 'inspector';
 
 
 
@@ -9,14 +10,30 @@ import { Button } from 'reactstrap';
         super(props)
         this.state = {
             quantity: 1,
+            productImages: [],
+            counter: 0
         }
-        
+        this.productImages = []
+        this.imageStyles = {
+            width: '300px',
+            height: '300px', 
+            backgroundImage: undefined,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }
       }
     
     componentDidMount() {
         this.props.getSingleProduct(this.props.match.params.id)
     }
 
+    componentWillReceiveProps(nextProps){
+        this.imageStyles = {
+            ...this.imageStyles,
+            backgroundImage: `url(${nextProps.singleProduct.image})`
+        }
+    }
 
     handleChange = (e) => {
         const {name, value} = e.target;
@@ -40,44 +57,83 @@ import { Button } from 'reactstrap';
 
     handleAddWishlist = () => {
         let wishlistItem =  { 'sku': this.props.selectedItemSku, 'quantity': parseInt(this.state.quantity) }
-    //     this.props.handleAddItemToWishlist(wishlistItem)
+      this.props.handleAddItemToWishlist(wishlistItem)
     }
     
     handleAddCart = () => {
         
     }
-    
-    render(){
-        let imageStyles = {
-            width: '300px',
-            height: '300px', 
-            backgroundImage: `url(${this.props.singleProduct.image})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat'
-        }
-        const {name, shortDescription, regularPrice, longDescription} = this.props.singleProduct
 
+    createImageArray = () => {
+        const allImageArray = this.props.singleProduct.images
+        allImageArray.forEach(item => {
+            this.productImages.push(item.href)
+        })
+        console.log(this.productImages)
+    }
+   
+
+    handleImageScrollRight = () => {
+        console.log('click works')
+        this.imageStyles = {
+            ...this.imageStyles,
+            backgroundImage: `url(${this.productImages[2]})`
+        }
+        this.setState({
+            counter: +1
+        })
+    }
+        
+    render(){
+        // let imageStyles = {
+        //     width: '300px',
+        //     height: '300px', 
+        //     backgroundImage: `url(${this.props.singleProduct.image})`,
+        //     backgroundPosition: 'center',
+        //     backgroundSize: 'cover',
+        //     backgroundRepeat: 'no-repeat'
+        // }
+        
+        const {name, shortDescription, regularPrice, longDescription, customerReviewAverage, customerReviewCount, depth, height, modelNumber} = this.props.singleProduct
+        
+        this.props.singleProduct && this.props.singleProduct.images && this.createImageArray()
+        
         return (
-            <div>
+            
+            <div className='productDetailWrapper' >
                 <h2> {name}</h2>
                 <h4>{shortDescription}</h4>
-                <div style={imageStyles}></div>
-                <h6>${regularPrice}</h6>
-                <p>{longDescription}</p>
-                <form name='quantityForm'>
-                    <input  name='quantity'
-                            type='number' 
-                            placeholder='1'
-                            min='0' 
-                            step='1'
-                            value={this.state.quantity}
-                            onChange={this.handleChange}
-                            onKeyDown={this.handleKeypress}>
-                    </input>
-                </form>
-            <Button color="secondary" onClick={this.handleAddWishlist}>Add to Wishlist</Button>
-            <Button color="secondary" onClick={this.handleAddCart}>Add to Cart</Button>{' '}
+                <div className='productGridContainer'>
+                    <div style={this.imageStyles}></div>
+                    <div className='productGridRows'>
+                        <h6 className='descriptionHeader'>Product Description:</h6>
+                        <p>{longDescription}</p>
+                        <p>Model Number: {modelNumber}</p>
+                        <h6>Price: ${regularPrice}</h6>
+                        <h6>Average Rating: {customerReviewAverage}</h6>
+                        <h6>Reviews: {customerReviewCount}</h6>
+                        <h6>Dimensions: {depth} x {height}</h6>
+                    </div>
+                    <div className='productForm'>
+                        <button onClick={this.handleImageScrollRight} >button</button>
+                    <form  name='quantityForm'>
+                        Quantity: <input  name='quantity'
+                                type='number' 
+                                placeholder='1'
+                                min='0' 
+                                step='1'
+                                value={this.state.quantity}
+                                onChange={this.handleChange}
+                                onKeyDown={this.handleKeypress}>
+                        </input>
+                    </form>
+                    <div className='buttonContainer'>
+                        <Button className='addButton' color="secondary" onClick={this.handleAddWishlist}>Add to Wishlist</Button>
+                        <Button className='addButton' color="secondary" onClick={this.handleAddCart}>Add to Cart</Button>{' '}
+                    </div>
+                </div>
+                </div>
+               
             </div>
         );
     }
